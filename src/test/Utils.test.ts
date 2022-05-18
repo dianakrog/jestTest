967837c7706e6afeab1a24be252d1bc29907a1ff
Diana Gutierrez <diana.gutierrezc@perficient.com>
2022-05-18 16:06:35 -0500

    complete seconth seccion

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,7 +1,11 @@
 import { userInfo } from 'os';
 import {Utils} from '../app/Utils'
 
-describe ('Utils test suite', ()=>{
+describe.only ('Utils test suite', ()=>{
+
+    beforeEach(()=>{
+        console.log('before each message');
+    })
 
     test('first test', ()=>{
         const result = Utils.toUpperCase('abc');
@@ -28,4 +32,49 @@ describe ('Utils test suite', ()=>{
         expect(parseUrl.query).toEqual(expectedQuery);
         expect(expectedQuery).toBe(expectedQuery);
     });
+    
+    test.skip('Parse simple URL validationUser', () => {
+        const url = 'http://localhost:8080/validationUser'
+        const parseUrl = Utils.parseUrl(url);
+        expect(parseUrl.href).toBe(url);
+        expect(parseUrl.port).toBe('8080');
+        expect(parseUrl.protocol).toBe('http:');
+        expect(parseUrl.query).toEqual({});
+    });
+
+    test('Test Invalida URL', ()=> {
+        function expectError() {
+            Utils.parseUrl('')
+        }
+        expect(expectError).toThrowError();
+    });
+
+    test('Test Invalida URL with error function', ()=> {
+        expect(() => {
+            Utils.parseUrl('')
+        }).toThrowError();
+    });
+
+    test.only('Test Invalida URL with try catch', ()=> {
+        try {
+            Utils.parseUrl('');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error).toHaveProperty('message', 'Empty url!')
+        }
+    });
+
+
+});
+
+describe ('Utils test suite 2', ()=>{
+
+    beforeAll(()=>{
+        console.log('before all message');
+    })
+
+    test('first test 2', ()=>{
+        const result = Utils.toUpperCase('abc');
+        expect(result).toBe('ABC');
+    });   
 });
